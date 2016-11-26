f0d7594ae93e2ad22b9b8d9e6e59d19f93670f04
spencermountain <spencermountain@gmail.com>
2016-11-26 11:17:32 -0500

    yolo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import styler from 'react-styling'
 import Thing from './thing'
 import colors from './colors'
 import './index.css'
-import CNTower from "./cntower.svg"
 
 const assets={
-  // CNTower:require("./cntower.svg"),
-  goto:require("../assets/goto.jpeg"),
+  CNTower:require("./cntower.svg"),
+  goto:require("../assets/goto9.png"),
   argmap:require("../assets/argmap.png"),
 }
 const style = styler`
@@ -32,37 +31,55 @@ const style = styler`
         border-radius:5px
 
   about:
+    position:relative;
+    greeting:
+      position:absolute;
+      font-size:11
+      left:-50
+      top:-55
+      font-size:24
+      color:dimgrey
+      text-align:center
     name:
       font-size:35px
       color:darkslategrey
-
-  byline:
-    text-align:left
-    margin:10px
-    marginLeft:25px
-
-  cntower:
-    height:120
-  bio:
-    position:relative
-    border:1px solid red
-    height:100px
-    width:65%
+      margin:10px
+      margin-bottom:5px
+      marginLeft:25px
+      text-align:left
     description:
-      position:absolute
-      left:10
-      top:10
       color:grey
+      text-align:left
+      margin-left:55
+      z-index:1
+      cntower:
+        position:absolute
+        z-index:4
+        top:-20
+        left:240
+        height:90
+    color:
+      margin:10
+      marginLeft:25
+      height:10
+      marginRight:15%
 
   projects:
-    border:1px solid grey
-    margin:20px
+    margin:8%
     marginTop:0px
     project:
       display: flex;
       flex-wrap: nowrap;
-      min-height:150px
-      circle:
+      min-height:200px
+      side:
+        border:1px solid grey
+        flex-basis:75px
+      about:
+        border:1px solid grey
+        flex-basis:400px
+      img:
+        border:1px solid grey
+        flex-basis:400px
 
   water:
     position:fixed
@@ -92,23 +109,33 @@ class App extends Component {
           </div>
 
           <div style={css.about}>
-            <div style={css.about.name}>Ŝpeńcëѓ Қełƚy</div>
-            <div style={css.about.description}>
-              <div>
-              {'freelance javascript developer'}
-              <img style={css.about.cntower} src={CNTower}/>
-              </div>
+            <div style={css.about.greeting}>
+              <div style={{fontSize:14}}>{'oh,'}</div>
+              <div style={{fontSize:44, borderBottom:'1px solid lightsteelblue', margin:0}}>{'  hi'}</div>
             </div>
-            <Thing colors={colors[0]}/>
+            <div style={css.about.name}>
+              {'Sҏeƞceȓ Қellӯ'}
+            </div>
+            <div style={css.about.description}>
+              <img src={assets.CNTower} style={css.about.description.cntower}/>
+              {'freelance developer, toronto'}
+            </div>
+            <div style={css.about.color}>
+              <Thing colors={colors[0]}/>
+            </div>
           </div>
-
           <div style={css.projects}>
             <div style={css.projects.project}>
-
+              <div style={css.projects.project.side}>
+              </div>
+              <div style={css.projects.project.about}>
+              </div>
+              <div style={css.projects.project.img}>
+              </div>
             </div>
+
           </div>
         </div>
-        <Thing colors={colors[1]}/>
         <div style={css.water}/>
       </div>
     );
