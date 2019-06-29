5ba58f299ec638b59ef76b1a367db702e0ec187f
spencer kelly <spencermountain@gmail.com>
2019-06-28 20:36:58 -0400

    work together

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const htm = require('htm')
 const h = htm.bind(require('vhtml'))
 // const generate = require('./generate')
 const interesting = require('../interesting')
+const shrinkGrid = require('./shrink-grid')
 
 // let cells = generate()
 // console.log(JSON.stringify(cells))
@@ -13,20 +14,22 @@ const drawGrid = function(cells) {
     if (cell.image) {
       style += `background-image:url(${cell.image});`
     }
-    return h`<div class=${classes} style=${style}></div>`
+    return h`<div class=${classes} style=${style}>${cell.text}</div>`
   })
   return cells
 }
+
+document.querySelector('#topGrid').innerHTML = shrinkGrid()
 
 let grid = document.querySelector('#showAll')
 
 let html = interesting
   .map((cells, i) => {
-    return h`<div>${i}<div class="goldGrid h8 mv4">
+    return h`<div class="goldGrid mv6">
  ${drawGrid(cells)}
-</div></div>
+</div>
 `
   })
   .join(' ')
-console.log(html)
+
 grid.innerHTML = html
