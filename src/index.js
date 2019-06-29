8c072688d25e09ae1d5617f567d99bce0343f441
spencer kelly <spencermountain@gmail.com>
2019-06-29 14:41:49 -0400

    cleanup html

--- src/index.js
+++ src/index.js
@@ -1,57 +1,6 @@
-const htm = require('htm')
-const h = htm.bind(require('vhtml'))
 // const generate = require('./generate')
-const interesting = require('../interesting')
-const shrinkGrid = require('./shrink-grid')
 
-require('intersection-observer')
-const scrollama = require('scrollama')
-
-// instantiate the scrollama
-const scroller = scrollama()
-
-// setup the instance, pass callback functions
-scroller
-  .setup({
-    step: '.step'
-  })
-  .onStepEnter(response => {
-    // console.log(response)
-    // { element, index, direction }
-  })
-  .onStepExit(response => {
-    // { element, index, direction }
-  })
-
-// setup resize event
-window.addEventListener('resize', scroller.resize)
+require('./scroll')
 
 // let cells = generate()
 // console.log(JSON.stringify(cells))
-
-const drawGrid = function(cells) {
-  cells = cells.map(cell => {
-    let classes = cell.classes.join(' ')
-    let style = ''
-    if (cell.image) {
-      style += `background-image:url(${cell.image});`
-    }
-    return h`<div class=${classes} style=${style}>${cell.text}</div>`
-  })
-  return cells
-}
-
-document.querySelector('#topGrid').innerHTML = shrinkGrid()
-
-let grid = document.querySelector('#showAll')
-
-let html = interesting
-  .map((cells, i) => {
-    return h`<div class="goldGrid mv6">
- ${drawGrid(cells)}
-</div>
-`
-  })
-  .join(' ')
-
-grid.innerHTML = html
