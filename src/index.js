12af2a4b1e808ffa511e3f0086b382b25f77ba4d
spencer kelly <spencermountain@gmail.com>
2019-06-29 12:15:07 -0400

    more content sections

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ const h = htm.bind(require('vhtml'))
 const interesting = require('../interesting')
 const shrinkGrid = require('./shrink-grid')
 
+require('intersection-observer')
+const scrollama = require('scrollama')
+
+// instantiate the scrollama
+const scroller = scrollama()
+
+// setup the instance, pass callback functions
+scroller
+  .setup({
+    step: '.step'
+  })
+  .onStepEnter(response => {
+    // console.log(response)
+    // { element, index, direction }
+  })
+  .onStepExit(response => {
+    // { element, index, direction }
+  })
+
+// setup resize event
+window.addEventListener('resize', scroller.resize)
+
 // let cells = generate()
 // console.log(JSON.stringify(cells))
 
