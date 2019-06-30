c97cbc8323f9285675af098246cb34cd546b136f
spencer kelly <spencermountain@gmail.com>
2019-06-29 21:30:31 -0400

    some events firing

--- src/scroll.js
+++ src/scroll.js
@@ -2,12 +2,55 @@ require('intersection-observer')
 const scrollama = require('scrollama')
 
 const scroller = scrollama()
+const sideblue = document.querySelector('#slideblue')
+const hidesky = document.querySelector('#hidesky')
+const saddam = document.querySelector('#saddam')
+
 scroller
   .setup({
-    step: '.step'
+    step: '.trigger'
   })
-  .onStepEnter(resp => {})
-  .onStepExit(resp => {})
+  .onStepEnter(resp => {
+    if (resp.direction === 'down') {
+      console.log(resp.element.id)
+      if (resp.element.id === 'trigger1') {
+        sideblue.style.left = '0%'
+        hidesky.style.left = '50%'
+      }
+      if (resp.element.id === 'hide_earth') {
+        resp.element.style['background-image'] = 'url(./assets/misc/hay.jpg)'
+        setTimeout(() => {
+          saddam.style['background-image'] = 'url(./assets/misc/vancouver.jpg)'
+        }, 500)
+      }
+      if (resp.element.id === 'shrinkblue') {
+        resp.element.style.width = '50%'
+      }
+      if (resp.element.id === 'growpink') {
+        resp.element.style.width = '50%'
+      }
+    }
+  })
+  .onStepExit(resp => {
+    if (resp.direction === 'up') {
+      if (resp.element.id === 'trigger1') {
+        sideblue.style.left = '100%'
+        hidesky.style.left = '0%'
+      }
+      if (resp.element.id === 'hide_earth') {
+        resp.element.style['background-image'] = 'url(./assets/misc/satellite.jpg)'
+        setTimeout(() => {
+          saddam.style['background-image'] = 'url(./assets/misc/saddam.jpg)'
+        }, 500)
+      }
+      if (resp.element.id === 'shrinkblue') {
+        resp.element.style.width = '100%'
+      }
+      if (resp.element.id === 'growpink') {
+        resp.element.style.width = '100%'
+      }
+    }
+  })
 
 // setup resize event
 window.addEventListener('resize', scroller.resize)
