645c5c43c340c7aa537b456f45b17f2e688a4cfd
spencer kelly <spencermountain@gmail.com>
2020-06-21 17:03:06 -0400

    start new portfolio page

---
+++ portfolio/components/Mov.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let src = ''
+  export let caption = ''
+  export let link = ''
+  export let width = '100%'
+</script>
+
+<style>
+  .container {
+    text-align: right;
+  }
+  .caption {
+    font-size: 0.9rem;
+  }
+  .link {
+    text-decoration: none;
+  }
+  video {
+    max-width: 100%;
+  }
+</style>
+
+<div class="container">
+  {#if link}
+    <a href={link} class="link" target="_blank">
+      <video style="width:{width}; margin-bottom:0px;" {src} autoplay mute loop />
+    </a>
+  {:else}
+    <video style="width:{width}; margin-bottom:0px;" {src} autoplay mute loop />
+  {/if}
+  <div class="caption">
+    {@html caption}
+  </div>
+</div>
---
+++ scripts/watch.js
@@ -0,0 +1,38 @@
+const chokidar = require('chokidar')
+const buildOne = require('./build')
+const server = require('./server')
+const path = require('path')
+const post = process.argv[2] || ''
+
+let abs = path.join(__dirname, '../')
+if (post) {
+  abs = path.join(abs, `/${post}`)
+}
+server(abs)
+const doit = function () {
+  buildOne(abs)
+}
+
+const watcher = chokidar
+  .watch([`${abs}/**/*.svelte`, `${abs}/build/*.html`], {
+    ignored: [/node_modules/, /(^|[\/\\])\../],
+    persistent: true
+  })
+  .on('ready', () => {
+    doit()
+    watcher.on('add', () => {
+      doit()
+    })
+    watcher.on('addDir', () => {
+      doit()
+    })
+  })
+  .on('change', () => {
+    doit()
+  })
+  .on('unlink', () => {
+    doit()
+  })
+  .on('unlinkDir', () => {
+    doit()
+  })
