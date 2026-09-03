dd0d50744ead0e6ad7843b5ac0a4502356ab3164
spencer kelly <spencerkelly86@gmail.com>
2026-09-03 10:57:07 -0400

    start graph

---
+++ pages/portfolio/graph/events.js
@@ -0,0 +1,12 @@
+const events = [
+  {
+    title: 'Gradudated',
+    date: 'June 2008'
+  },
+  {
+    title: 'Grad School',
+    date: 'Sep 2010'
+  }
+]
+
+export default events
---
+++ pages/portfolio/graph/jobs.js
@@ -0,0 +1,94 @@
+const jobs = [
+  {
+    label: '',
+    start: 'Nov 2004',
+    end: 'Dec 2008',
+    description: 'University',
+    detail: '',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'hmm',
+    start: 'Nov 2008',
+    end: 'Dec 2011',
+    description: '',
+    detail: '',
+    height: '50%',
+    labelPosition: 'above',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'State.com',
+    start: 'Nov 2010',
+    end: 'Dec 2014',
+    description: 'Social network',
+    detail: 'Rails/Node REST api, design prototyping',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'Govinvest',
+    start: 'May 2015',
+    end: 'Dec 2017',
+    description: 'Pension liability analysis',
+    detail: 'React + D3.js infographics dashboard',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'Small Wins',
+    start: 'Dec 2015',
+    end: 'Apr 2018',
+    description: 'Task-management chatbot',
+    detail: 'CI/CD for desktop app, timezone library',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'Venngage',
+    start: 'Jan 2019',
+    end: 'July 2019',
+    description: 'Infographics studio',
+    detail: 'Custom web infographics for corporate clients',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'MBI Health',
+    start: 'July 2019',
+    end: 'May 2020',
+    description: 'NHS medical text analysis',
+    detail: 'TDD test-suite, for critical production QA',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'Fluent',
+    start: 'May 2020',
+    end: 'Oct 2023',
+    description: 'Language-learning application',
+    detail: 'Client-side translation of internet text',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  },
+  {
+    label: 'City of Toronto',
+    start: 'March 2024',
+    end: 'Aug 2026',
+    description: 'Transportation analytics',
+    detail: 'Visualization of traffic volumes and collisions',
+    labelPosition: 'inline',
+    color: null,
+    row: 1
+  }
+]
+export default jobs
